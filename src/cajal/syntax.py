c5bcbb286a7ae2f15cff78071fa2e751ec2ed869
Joey Velez-Ginorio <joeyv@seas.upenn.edu>
2025-10-01 22:00:20 -0400

    Add values, remove unnecessary dataclass attributes

--- src/cajal/syntax.py
+++ src/cajal/syntax.py
@@ -1,42 +1,66 @@
 from dataclasses import dataclass
+from collections.abc import Mapping
 
 # ------------------------------------- Types ------------------------------------- #
+
 type Ty = TyBool | TyFun
 
-@dataclass(eq=True, frozen=True, unsafe_hash=True)
+@dataclass
 class TyBool: ...
 
-@dataclass(eq=True, frozen=True, unsafe_hash=True)
+@dataclass
 class TyFun: 
     ty1 : Ty
     ty2 : Ty
 
 # ------------------------------------- Terms ------------------------------------- #
+
 type Tm = TmVar | TmTrue | TmFalse | TmIf | TmFun | TmApp
 
-@dataclass(eq=True, frozen=True, unsafe_hash=True)
+@dataclass
 class TmVar:
     name: str
 
-@dataclass(eq=True, frozen=True, unsafe_hash=True)
+@dataclass
 class TmTrue: ...
 
-@dataclass(eq=True, frozen=True, unsafe_hash=True)
+@dataclass
 class TmFalse: ...
 
-@dataclass(eq=True, frozen=True, unsafe_hash=True)
+@dataclass
 class TmIf:
     tm1: Tm
     tm2: Tm
     tm3: Tm
 
-@dataclass(eq=True, frozen=True, unsafe_hash=True)
+@dataclass
 class TmFun:
     name: str
     ty: Ty
     tm: Tm
 
-@dataclass(eq=True, frozen=True, unsafe_hash=True)
+@dataclass
 class TmApp:
     tm1 : Tm
     tm2 : Tm
+
+
+# ------------------------------------- Values ------------------------------------- #
+
+type Val = VTrue | VFalse | VClosure
+
+@dataclass
+class VTrue: ...
+
+@dataclass
+class VFalse: ...
+
+type Env = Mapping[str, tuple[Val, Ty]]
+
+@dataclass
+class VClosure:
+    name: str
+    ty: Ty
+    tm: Tm
+    env: Env
+
