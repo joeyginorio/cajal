28f955cea985098fc7390ae32757d0466094b6bc
Joey Velez-Ginorio <joeyv@seas.upenn.edu>
2025-10-02 13:26:15 -0500

    Rename to avoid nameclash with native python eval

--- src/cajal/evaluating.py
+++ src/cajal/evaluating.py
@@ -1,6 +1,6 @@
 from cajal.syntax import *
 
-def eval(tm: Ty, env: Env) -> Val:
+def evaluate(tm: Ty, env: Env) -> Val:
     match tm:
         case TmVar(x):
             v, _ = env[x]
@@ -12,20 +12,20 @@ def eval(tm: Ty, env: Env) -> Val:
         case TmFun(x, ty, tm):
             return VClosure(x, ty, tm, env)
         case TmIf(tm1, tm2, tm3):
-            v1 = eval(tm1, env)
+            v1 = evaluate(tm1, env)
             match v1:
                 case VTrue():
-                    return eval(tm2,env)
+                    return evaluate(tm2,env)
                 case VFalse():
-                    return eval(tm3, env)
+                    return evaluate(tm3, env)
                 case _:
                     raise TypError(f"TmIf: {tm1=} doesn't evaluate to a boolean, {v1=}.")
         case TmApp(tm1, tm2):
-            v1 = eval(tm1, env)
-            v2 = eval(tm2, env)
+            v1 = evaluate(tm1, env)
+            v2 = evaluate(tm2, env)
             match v1:
                 case VClosure(x, ty, tm, c_env):
                     c_env |= {x: (v2, ty)}
-                    return eval(tm, c_env)
+                    return evaluate(tm, c_env)
                 case _:
                     raise TypeError(f"TmApp: {tm1=} doesn't evaluate to a closure, {v1=}.")
