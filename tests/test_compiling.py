fc5f2b937aa0ae2faa744fd03cae58f8957c3ccb
Joey Velez-Ginorio <joeyv@seas.upenn.edu>
2025-10-05 18:36:35 -0500

    Added unit test for gradients of NOT and property-based test for checking whether gradients exist for compiled programs

--- tests/test_compiling.py
+++ tests/test_compiling.py
@@ -1,14 +1,16 @@
 from hypothesis import given, settings, HealthCheck, Verbosity, Phase
-from strategies import gen_closed_prog_observable
+from strategies import gen_closed_prog_observable, gen_closed_prog_observable_except
 from cajal.compiling import compile, compile_val
 from cajal.evaluating import evaluate
+from cajal.syntax import *
+from torch import tensor, isfinite
 
 @settings(max_examples=100, 
           suppress_health_check=[HealthCheck.too_slow], 
           verbosity=Verbosity.normal,
           deadline=None)
 @given(gen_closed_prog_observable())
-def test_compile(ctx_tm_ty):
+def test_compiler_correctness(ctx_tm_ty):
 
     _, tm, _ = ctx_tm_ty
     v = evaluate(tm, {})
@@ -17,3 +19,29 @@ def test_compile(ctx_tm_ty):
     compiled_v = compile_val(v)
 
     assert all(compiled_tm == compiled_v)
+
+@settings(max_examples=100, 
+          suppress_health_check=[HealthCheck.too_slow], 
+          verbosity=Verbosity.normal,
+          deadline=None)
+@given(gen_closed_prog_observable_except({'x': TyBool()}))
+def test_grad_exists(ctx_tm_ty):
+    _, tm, _ = ctx_tm_ty
+    env = {'x' : tensor([1., 2.], requires_grad=True)}
+    y = compile(tm, env)
+    y.sum().backward()
+    assert all(isfinite(env['x'].grad))
+
+def test_grad_not():
+    ''' 
+    f(x) = x[0] * [0,1]^T + x[1] * [1,0]^T
+    df/x0 = [0,1]^T
+    df/x1 = [1,0]^T
+    df/dx = [0, 1]
+          = [1, 0] 
+    '''
+    env = {'x' : tensor([1., 2.], requires_grad=True)}
+    y = compile(TmIf(TmVar('x'), TmFalse(), TmTrue()), env)
+    y.sum().backward()
+    assert all(env['x'].grad == tensor([1., 1.]))
+
