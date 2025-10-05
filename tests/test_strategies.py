404f35a6bd3db592b2870569b4f1cc58f873789a
Joey Velez-Ginorio <joeyv@seas.upenn.edu>
2025-10-04 20:29:29 -0500

    Add tests to check that open and closed observable program generators generate programs of type Bool

--- tests/test_strategies.py
+++ tests/test_strategies.py
@@ -1,10 +1,25 @@
 from hypothesis import given, settings, HealthCheck, Verbosity
-from strategies import gen_prog
+from strategies import gen_prog, gen_prog_observable, gen_closed_prog_observable
 from cajal.typing import check
+from cajal.syntax import TyBool
 
-@settings(max_examples=50, suppress_health_check=[HealthCheck.too_slow], verbosity=Verbosity.normal)
+@settings(max_examples=100, suppress_health_check=[HealthCheck.too_slow], verbosity=Verbosity.normal)
 @given(gen_prog())
 def test_gen_prog(ctx_tm_ty):
     ctx, tm, ty = ctx_tm_ty
     ty_check = check(tm, ctx)
     assert ty == ty_check
+
+@settings(max_examples=100, suppress_health_check=[HealthCheck.too_slow], verbosity=Verbosity.normal)
+@given(gen_prog_observable())
+def test_gen_prog_observable(ctx_tm_ty):
+    ctx, tm, ty = ctx_tm_ty
+    ty_check = check(tm, ctx)
+    assert ty == ty_check == TyBool()
+
+@settings(max_examples=100, suppress_health_check=[HealthCheck.too_slow], verbosity=Verbosity.normal)
+@given(gen_closed_prog_observable())
+def test_gen_closed_prog_observable(ctx_tm_ty):
+    ctx, tm, ty = ctx_tm_ty
+    ty_check = check(tm, ctx)
+    assert ty == ty_check == TyBool()
