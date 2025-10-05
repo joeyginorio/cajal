27f9d697e5ccbf4903a50a29182f5298b46088a8
Joey Velez-Ginorio <joeyv@seas.upenn.edu>
2025-10-04 20:28:57 -0500

    Add test for compiler correctness

--- tests/test_compiling.py
+++ tests/test_compiling.py
@@ -1,10 +1,19 @@
-from hypothesis import given, settings, HealthCheck, Verbosity
-from strategies import gen_closed_prog
-from cajal.compiling import compile
+from hypothesis import given, settings, HealthCheck, Verbosity, Phase
+from strategies import gen_closed_prog_observable
+from cajal.compiling import compile, compile_val
+from cajal.evaluating import evaluate
 
-@settings(max_examples=5, suppress_health_check=[HealthCheck.too_slow], verbosity=Verbosity.normal)
-@given(gen_closed_prog())
+@settings(max_examples=100, 
+          suppress_health_check=[HealthCheck.too_slow], 
+          verbosity=Verbosity.normal,
+          deadline=None)
+@given(gen_closed_prog_observable())
 def test_compile(ctx_tm_ty):
-    _, tm, ty = ctx_tm_ty
+
+    _, tm, _ = ctx_tm_ty
+    v = evaluate(tm, {})
+
     compiled_tm = compile(tm, {})
-    assert 1 == 1
+    compiled_v = compile_val(v)
+
+    assert all(compiled_tm == compiled_v)
