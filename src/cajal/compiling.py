7a388b3a9e7fd02ff181d6d2a11e040556b41fd3
Joey Velez-Ginorio <joeyv@seas.upenn.edu>
2025-10-06 15:57:09 -0400

    Add device global for correctly setting tensors

--- src/cajal/compiling.py
+++ src/cajal/compiling.py
@@ -1,16 +1,25 @@
+import torch
 from collections.abc import Mapping
 from torch import tensor, Tensor
 from cajal.typing import *
 from functools import partial
+
+# TODO: Refactor compile and tests to take device as argument
+if torch.backends.mps.is_available():
+    device = torch.device("mps")
+elif torch.cuda.is_available():
+    device = torch.device("cuda")
+else:
+    device = torch.device("cpu")
 
 def compile(tm: Tm, env):
     match tm:
         case TmVar(x):
             return env[x]
         case TmTrue():
-            return tensor([1.,0.])
+            return tensor([1.,0.], requires_grad=True, device=device)
         case TmFalse():
-            return tensor([0.,1.])
+            return tensor([0.,1.], requires_grad=True, device=device)
         case TmIf(tm1, tm2, tm3):
             b = compile(tm1, env)
             branch_if = compile(tm2, env)
@@ -19,6 +28,7 @@ def compile(tm: Tm, env):
         case TmFun(x, _, tm):
             return lambda arg, env=env: compile(tm, env | {x: arg})
         case TmApp(tm1, tm2):
+            # Maybe dispatch on whether tm1 is function/matrix
             f_closure = compile(tm1, env)
             arg = compile(tm2, env)
             return f_closure(arg)
