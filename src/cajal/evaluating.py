0bf87786ecdeaba2097080126a166cd03e0be100
Joey Velez-Ginorio <joeyv@seas.upenn.edu>
2025-10-06 23:54:48 -0400

    Add evaluation for Nat

--- src/cajal/evaluating.py
+++ src/cajal/evaluating.py
@@ -9,8 +9,24 @@ def evaluate(tm: Tm, env: Env) -> Val:
             return VTrue()
         case TmFalse():
             return VFalse()
+        case TmZero():
+            return VZero()
+        case TmSucc(tm):
+            v = evaluate(tm, env)
+            return VSucc(v)
         case TmFun(x, ty, tm):
             return VClosure(x, ty, tm, env)
+        case TmIter(tm1, name2, tm2, tm3):
+            v3 = evaluate(tm3, env)
+            match v3:
+                case VZero():
+                    return evaluate(tm1, env)
+                case VSucc(v):
+                    v_n_minus_1 = evaluate(TmIter(tm1, name2, tm2, v), env)
+                    v_n = evaluate(tm2, env | {name2: v_n_minus_1})
+                    return v_n
+                case _:
+                    raise TypeError(f"TmIter: {tm3=} doesn't evaluate to a nat, {v3=}.")
         case TmIf(tm1, tm2, tm3):
             v1 = evaluate(tm1, env)
             match v1:
@@ -29,3 +45,5 @@ def evaluate(tm: Tm, env: Env) -> Val:
                     return evaluate(tm, c_env)
                 case _:
                     raise TypeError(f"TmApp: {tm1=} doesn't evaluate to a closure, {v1=}.")
+        case _:
+            return tm
