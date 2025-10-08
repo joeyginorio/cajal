c37a918114e0a743ade8377da59d958e5b63fc4f
Joey Velez-Ginorio <joeyv@seas.upenn.edu>
2025-10-08 14:33:56 -0400

    Add newlines for formatting

--- src/cajal/compiling.py
+++ src/cajal/compiling.py
@@ -14,31 +14,39 @@ else:
 
 def compile(tm: Tm, env):
     match tm:
+
         case TmVar(x):
             return env[x]
+        
         case TmTrue():
             return tensor([1., 0.], requires_grad=True, device=device)
+        
         case TmFalse():
             return tensor([0., 1.], requires_grad=True, device=device)
+        
         case TmZero():
             return tensor([1.], requires_grad=True)
+        
         case TmSucc(tm):
             n = compile(tm, env)
             succ_n = torch.concat([torch.tensor([0.]), n])
             succ_n.retain_grad()
             return succ_n
+        
         case TmIf(tm1, tm2, tm3):
             b = compile(tm1, env)
             branch_if = compile(tm2, env)
             branch_else = compile(tm3, env)
             return b[0] * branch_if + b[1] * branch_else
+        
         case TmFun(x, _, tm):
             return lambda arg, env=env: compile(tm, env | {x: arg})
+        
         case TmApp(tm1, tm2):
             # Maybe dispatch on whether tm1 is function/matrix
             f_closure = compile(tm1, env)
             arg = compile(tm2, env)
-            return f_closure(arg)
+            return f_closure(arg)   
 
 
 def compile_val(v: Val):
