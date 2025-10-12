5af9b2d01129f50fee278e34d7712163faef2e9a
Joey Velez-Ginorio <joeyv@seas.upenn.edu>
2025-10-11 20:42:53 -0400

    Fix tests to use TypedTensor correctly

--- tests/test_compiling.py
+++ tests/test_compiling.py
@@ -1,6 +1,6 @@
 from hypothesis import given, settings, HealthCheck, Verbosity, Phase
 from strategies import gen_closed_prog_observable, gen_closed_prog_observable_except
-from cajal.compiling import compile, compile_val
+from cajal.compiling import compile, compile_val, TypedTensor
 from cajal.evaluating import evaluate
 from cajal.syntax import *
 from torch import tensor, isfinite
@@ -17,7 +17,7 @@ def test_compiler_correctness(ctx_tm_ty):
 
     compiled_tm = compile(tm)({})
     compiled_v = compile_val(v)({})
-    assert all(compiled_tm == compiled_v)
+    assert compiled_tm == compiled_v
 
 @settings(max_examples=100, 
           suppress_health_check=[HealthCheck.too_slow],
@@ -26,10 +26,10 @@ def test_compiler_correctness(ctx_tm_ty):
 @given(gen_closed_prog_observable_except({'x': TyBool()}))
 def test_grad_exists(ctx_tm_ty):
     _, tm, _ = ctx_tm_ty
-    env = {'x' : tensor([1., 2.], requires_grad=True)}
+    env = {'x' : TypedTensor(tensor([1., 2.], requires_grad=True), TyBool())}
     y = compile(tm)(env)
-    y.sum().backward()
-    assert all(isfinite(env['x'].grad))
+    y.data.sum().backward()
+    assert all(isfinite(env['x'].data.grad))
 
 def test_grad_not():
     '''
@@ -39,8 +39,8 @@ def test_grad_not():
     df/dx = [0, 1]
           = [1, 0] 
     '''
-    env = {'x' : tensor([1., 2.], requires_grad=True)}
+    env = {'x' : TypedTensor(tensor([1., 2.], requires_grad=True), TyBool())}
     y = compile(TmIf(TmVar('x'), TmFalse(), TmTrue()))(env)
-    y.sum().backward()
-    assert all(env['x'].grad == tensor([1., 1.]))
+    y.data.sum().backward()
+    assert all(env['x'].data.grad == tensor([1., 1.]))
 
