c6da67bdc3d8e66021cb2240ae6d95953e610b79
Joey Velez-Ginorio <joeyv@seas.upenn.edu>
2025-10-23 11:41:53 -0500

    Rename to dilate

--- experiments/dilate/model_indirect.py
+++ experiments/dilate/model_indirect.py
@@ -24,92 +24,46 @@ else:
 print(f"Using device: {device}")
 
 # ---------- Data --------------------
-train_xs = torch.load("experiments/rotate/data/train_xs.pt")
-train_ys = torch.load("experiments/rotate/data/train_ys.pt")
+train_xs = torch.load("experiments/dilate/data/train_xs.pt")
+train_ys = torch.load("experiments/dilate/data/train_ys.pt")
 
-test_xs = torch.load("experiments/rotate/data/test_xs.pt")
-test_ys = torch.load("experiments/rotate/data/test_ys.pt")
+test_xs = torch.load("experiments/dilate/data/test_xs.pt")
+test_ys = torch.load("experiments/dilate/data/test_ys.pt")
 
 train_ds = TensorDataset(train_xs, train_ys)
 test_ds = TensorDataset(test_xs, test_ys)
 
 # ---------- Model -------------------
-class ModelD(nn.Module):
+class ModelI(nn.Module):
     def __init__(self):
         super().__init__()
-
-        # R(784) -> R(2)
-        # 80k params
-        self.check = nn.Sequential(
+        self.net = nn.Sequential(
             nn.Flatten(),
-            nn.Linear(28 * 28, 100),
+            nn.Unflatten(1, (1,28,28)),
+            nn.Conv2d(1,6,5,padding=2),
             nn.ReLU(),
-            nn.Linear(100, 2),
-        )
-
-        # R(784) -> R(784)
-        # 600k params (no need to learn)
-        layer = nn.Linear(784, 784, bias=True)
-        nn.init.eye_(layer.weight)
-        nn.init.zeros_(layer.bias)
-        layer.weight.requires_grad = False
-        layer.bias.requires_grad = False
-        self.if_branch = nn.Sequential(
-            nn.Flatten(),
-            layer,
-            nn.Unflatten(1, (1, 28, 28)),
-        )
-
-        # R(784) -> R(200)
-        # 300k params
-        self.then_branch = nn.Sequential(
-            nn.Flatten(),
-            nn.Linear(28 * 28, 300),
+            nn.Conv2d(6,6,5,padding=2),
             nn.ReLU(),
-            nn.Linear(300, 28 * 28),
-            nn.Unflatten(1, (1, 28, 28)),
+            nn.Conv2d(6,1,5,padding=2),
+            nn.Flatten(1,2)
         )
-        self.p = cj.TmIf(cj.TmVar('bv'), cj.TmVar('ifv'), cj.TmVar('thenv'))
-        self.p = torch.vmap(compile(self.p), 
-                            in_dims=({'bv': (TypedTensor(0, None)),
-                                      'ifv': (TypedTensor(0, None)),
-                                      'thenv': (TypedTensor(0, None))},),
-                                      out_dims=TypedTensor(0, None))
-
 
     def forward(self, x):
-        b = self.check(x)
-        if_val = self.if_branch(x).view(-1, 1, 28 * 28)
-        then_val = self.then_branch(x).view(-1, 1, 28 * 28)
-        env = {'bv' : TypedTensor(b, cj.TyBool()),
-               'ifv' : TypedTensor(if_val, cj.TyBool()),
-               'thenv' : TypedTensor(then_val, cj.TyBool())}
-        return self.p(env).data.view(-1,28,28)
-
-    # @cajalwefwe
-    # def forward(self, x):
-    #     b = self.check(x)
-    #     if_val = self.if_branch(x)
-    #     else_val = self.then_branch(x)
-    #     if b:
-    #         return if_val
-    #     else:
-    #         return else_val
+        return self.net(x)
 
 # ---------- Training ----------------
 seeds = [0]
-# seeds = [0]
 # batch_sizes = [8,32,128,512]
-batch_sizes = [64,256,512,1024]
-# batch_sizes = [64]
-learning_rates = [.01, .001, .0001, .00001]
-# learning_rates = [.0001]
+# batch_sizes = [64,256,512,1024]
+batch_sizes = [64]
+# learning_rates = [.01, .001, .0001, .00001]
+learning_rates = [.001]
 idxs = list(range(20))
 
-# # CNN measurements
+# CNN measurements
 test_loader = DataLoader(test_ds, batch_size=2048, shuffle=False)
 # cnn = CNN().to(device)
-# cnn_path = "experiments/rotate/data/mnist_cnn.pt"
+# cnn_path = "experiments/dilate/data/mnist_cnn.pt"
 # cnn.load_state_dict(torch.load(cnn_path, map_location=device))
 # cnn.eval()
 
@@ -134,9 +88,9 @@ for seed in seeds:
             train_loader = DataLoader(train_ds, batch_size=batch_size, shuffle=True)
 
             # Initialize model 
-            modelD = ModelD().to(device)
+            modelI = ModelI().to(device)
             criterion = nn.L1Loss()
-            optimizer = optim.Adam(modelD.parameters(), lr=lr)
+            optimizer = optim.Adam(modelI.parameters(), lr=lr)
 
             # Calculate initial measurements
             test_loss = 0.0
@@ -145,12 +99,12 @@ for seed in seeds:
             with torch.no_grad():
                 # for data in one_loader:
                 #     data = data.to(device)
-                #     output = modelD(data)
+                #     output = modelI(data)
                 #     bpsnr = batch_psnr(output, data).mean().item()
                 for data, target in test_loader:
                     data, target = data.to(device), target.to(device)
 
-                    output = modelD(data)
+                    output = modelI(data)
                     bpsnr += batch_psnr(output, target).mean().item()
                     test_loss += criterion(output, target).item()
 
@@ -167,18 +121,18 @@ for seed in seeds:
 
             for idx in idxs:
                 x = test_ds[idx][0].unsqueeze(0).to(device)  # add batch dimension: (1,1,28,28)
-                output = modelD(x).squeeze().tolist()
+                output = modelI(x).squeeze().tolist()
                 output_test[(0, seed, batch_size, lr, idx)] = output
         
             step = 1
             freq = 100
-            for epoch in range(5):
+            for epoch in range(30):
                 print(f"Epoch: {epoch}, Lr: {lr}, Bs: {batch_size}, Seed: {seed}")
 
                 for data, target in train_loader:
                     data, target = data.to(device), target.to(device)
                     optimizer.zero_grad()
-                    output = modelD(data)
+                    output = modelI(data)
                     loss = criterion(output, target)
                     loss.backward()
                     optimizer.step()
@@ -199,7 +153,7 @@ for seed in seeds:
                             for data, target in test_loader:
                                 data, target = data.to(device), target.to(device)
 
-                                output = modelD(data)
+                                output = modelI(data)
                                 test_loss += criterion(output, target).item()
                                 bpsnr += batch_psnr(output, target).mean().item()
 
@@ -220,57 +174,57 @@ for seed in seeds:
                         # Record model outputs
                         for idx in idxs:
                             x = test_ds[idx][0].unsqueeze(0).to(device)  # add batch dimension: (1,1,28,28)
-                            output = modelD(x).squeeze().tolist()
+                            output = modelI(x).squeeze().tolist()
                             output_test[(0, seed, batch_size, lr, idx)] = output
 
 
 
-with open("experiments/rotate/data/direct_loss_train.csv", "w", newline="") as f:
-    writer = csv.writer(f)
-    writer.writerow(["step", "seed", "batch size", "lr", "loss"])
-    for (step, seed, batch_size, lr), loss in loss_train.items():
-        writer.writerow([step, seed, batch_size, lr, loss])
-with open("experiments/rotate/data/direct_loss_test.csv", "w", newline="") as f:
-    writer = csv.writer(f)
-    writer.writerow(["step", "seed", "batch size", "lr", "loss"])
-    for (step, seed, batch_size, lr), loss in loss_test.items():
-        writer.writerow([step, seed, batch_size, lr, loss])
-with open("experiments/rotate/data/direct_output_test.csv", "w", newline="") as f:
-    writer = csv.writer(f)
-    writer.writerow(["step", "seed", "batch size", "lr", "idx", "output"])
-    for (step, seed, batch_size, lr, idx), output in output_test.items():
-        writer.writerow([step, seed, batch_size, lr, idx, output])
-with open("experiments/rotate/data/direct_psnr_test.csv", "w", newline="") as f:
-    writer = csv.writer(f)
-    writer.writerow(["step", "seed", "batch size", "lr", "idx", "psnr"])
-    for (step, seed, batch_size, lr), snr in psnr_test.items():
-        writer.writerow([step, seed, batch_size, lr, idx, snr])
+# with open("experiments/dilate/data/indirect_loss_train.csv", "w", newline="") as f:
+#     writer = csv.writer(f)
+#     writer.writerow(["step", "seed", "batch size", "lr", "loss"])
+#     for (step, seed, batch_size, lr), loss in loss_train.items():
+#         writer.writerow([step, seed, batch_size, lr, loss])
+# with open("experiments/dilate/data/indirect_loss_test.csv", "w", newline="") as f:
+#     writer = csv.writer(f)
+#     writer.writerow(["step", "seed", "batch size", "lr", "loss"])
+#     for (step, seed, batch_size, lr), loss in loss_test.items():
+#         writer.writerow([step, seed, batch_size, lr, loss])
+# with open("experiments/dilate/data/indirect_output_test.csv", "w", newline="") as f:
+#     writer = csv.writer(f)
+#     writer.writerow(["step", "seed", "batch size", "lr", "idx", "output"])
+#     for (step, seed, batch_size, lr, idx), output in output_test.items():
+#         writer.writerow([step, seed, batch_size, lr, idx, output])
+# with open("experiments/dilate/data/indirect_psnr_test.csv", "w", newline="") as f:
+#     writer = csv.writer(f)
+#     writer.writerow(["step", "seed", "batch size", "lr", "idx", "psnr"])
+#     for (step, seed, batch_size, lr), snr in psnr_test.items():
+#         writer.writerow([step, seed, batch_size, lr, idx, snr])
 
 
-# idxs = [0, 1, 2, 3, 4, 5, 6]
+idxs = [8, 9, 10, 11, 12, 13, 14]
 
-# fig, axes = plt.subplots(nrows=7, ncols=2, figsize=(6, 8))
-# for row, idx in enumerate(idxs):
-#     # prepare input
-#     x = test_ds[idx][0]
-#     xim = x.squeeze().detach()
-#     with torch.no_grad():
-#         modelD.to('cpu')
-#         y = modelD(x.unsqueeze(0))
-#     yim = y.squeeze().detach()
+fig, axes = plt.subplots(nrows=7, ncols=2, figsize=(6, 8))
+for row, idx in enumerate(idxs):
+    # prepare input
+    x = test_ds[idx][0]
+    xim = x.squeeze().detach()
+    with torch.no_grad():
+        modelI.to('cpu')
+        y = modelI(x.unsqueeze(0))
+    yim = y.squeeze().detach()
 
-#     # plot input on the left, output on the right
-#     ax_in  = axes[row, 0]
-#     ax_out = axes[row, 1]
+    # plot input on the left, output on the right
+    ax_in  = axes[row, 0]
+    ax_out = axes[row, 1]
 
-#     ax_in.imshow(xim,  cmap="gray", vmin=0, vmax=1)
-#     ax_in.set_title(f"Input #{idx}")
-#     ax_in.axis("off")
+    ax_in.imshow(xim,  cmap="gray", vmin=0, vmax=1)
+    ax_in.set_title(f"Input #{idx}")
+    ax_in.axis("off")
 
-#     ax_out.imshow(yim, cmap="gray", vmin=0, vmax=1)
-#     ax_out.set_title(f"Output #{idx}")
-#     ax_out.axis("off")
+    ax_out.imshow(yim, cmap="gray", vmin=0, vmax=1)
+    ax_out.set_title(f"Output #{idx}")
+    ax_out.axis("off")
 
-# plt.tight_layout()
-# plt.show()
+plt.tight_layout()
+plt.show()
 
